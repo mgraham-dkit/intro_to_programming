4f266c6970ffbc1e3a8dd164d61dbcbf2b26c1d4
michelle <michelle.graham@dkit.ie>
2023-03-06 23:53:17 +0000

    Added extra check to confirm there is only one @ in the email address.
    Added a second version of the validate function that separates the checks into independent stages, rather than nesting the logic.

--- File IO/Exercise Solutions/CA_exercise_1-EmailValidator.py
+++ File IO/Exercise Solutions/CA_exercise_1-EmailValidator.py
@@ -1,12 +1,30 @@
 def validate_email(email):
     if "@" in email and "." in email:
-        email_components = email.split("@")
-        if len(email_components[0]) > 0 and len(email_components[1]) >= 3:
-            if "." in email_components[1]:
-                dot_comps = email_components[1].split(".")
-                if len(dot_comps[0]) > 0 and len(dot_comps[1]) > 0:
-                    return True
+        if email.count("@") == 1:
+            email_components = email.split("@")
+            if len(email_components[0]) > 0 and len(email_components[1]) >= 3:
+                if "." in email_components[1]:
+                    dot_comps = email_components[1].split(".")
+                    if len(dot_comps[0]) > 0 and len(dot_comps[1]) > 0:
+                        return True
     return False
 
+def validate_email_independent_steps(email):
+    if "@" not in email or "." not in email:
+        return False
+    if email.count("@") > 1:
+        return False
+    email_components = email.split("@")
+    if len(email_components[0]) == 0 or len(email_components[1]) < 3:
+        return False
+    if "." not in email_components[1]:
+        return False
+    dot_comps = email_components[1].split(".")
+    if len(dot_comps[0]) == 0 or len(dot_comps[1]) == 0:
+        return False
+    
+    return True
+
+
 email = "michelle@dkit.ie"
-print(validate_email(email))
+print(validate_email_independent_steps(email))
